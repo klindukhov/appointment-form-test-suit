99e2ad4f1ad3737881cc8b2ad15b243be5a64446
klindukhov <61867600+klindukhov@users.noreply.github.com>
2025-02-25 07:28:34 +0100

    fix waiting methods in scrollToTopTest

--- test/scrollToTopTest.mjs
+++ test/scrollToTopTest.mjs
@@ -11,12 +11,27 @@ describe("Scroll to top Button", async () => {
     const deltaY = Math.round((await footer.getRect()).y);
 
     await driver.actions().scroll(0, 0, 0, deltaY).perform();
+    let waitTime = 2000;
+    while (
+      (await driver.executeScript("return window.scrollY;")) < deltaY &&
+      waitTime > 0
+    ) {
+      await driver.manage().setTimeouts({ implicit: 200 });
+      waitTime -= 200;
+    }
   };
   const scrollToTop = async (driver) => {
     await driver.executeScript("window.scrollTo(0,0);");
+    while (
+      (await driver.executeScript("return window.scrollY;")) > 0 &&
+      waitTime > 0
+    ) {
+      await driver.manage().setTimeouts({ implicit: 200 });
+      waitTime -= 200;
+    }
   };
 
-  describe("For not logged in users", async () => {
+  context("For not logged in users", async () => {
     it("appears on all relevant pages after scrolling down", async () => {
       await driver.get("https://katalon-demo-cura.herokuapp.com/profile.php");
       const toTopButton = await driver.findElement(
@@ -27,23 +42,26 @@ describe("Scroll to top Button", async () => {
       expect(await toTopButton.isDisplayed()).to.be.true;
     });
     it("is not displayed when the page is not scrolled down", async () => {
+      for (const url of [
+        "https://katalon-demo-cura.herokuapp.com/profile.php",
+        "https://katalon-demo-cura.herokuapp.com/",
+      ]) {
+        await driver.get(url);
+        const toTopButton = await driver.findElement(
+          By.xpath('//*[@id="to-top"]')
+        );
+        expect(await toTopButton.isDisplayed()).to.be.false;
+      }
+    });
+    it("disappears after manually scrolling to top", async () => {
       await driver.get("https://katalon-demo-cura.herokuapp.com/profile.php");
       const toTopButton = await driver.findElement(
         By.xpath('//*[@id="to-top"]')
       );
-      expect(await toTopButton.isDisplayed()).to.be.false;
-
       await scrollDown(driver);
       await scrollToTop(driver);
       await driver.wait(until.elementIsNotVisible(toTopButton), 2000);
       expect(await toTopButton.isDisplayed()).to.be.false;
-
-      await driver.get("https://katalon-demo-cura.herokuapp.com/");
-      expect(
-        await (
-          await driver.findElement(By.xpath('//*[@id="to-top"]'))
-        ).isDisplayed()
-      ).to.be.false;
     });
     it("disappears after it is pressed", async () => {
       await driver.get("https://katalon-demo-cura.herokuapp.com/profile.php");
@@ -63,14 +81,21 @@ describe("Scroll to top Button", async () => {
       );
       await scrollDown(driver);
       await driver.wait(until.elementIsVisible(toTopButton), 2000);
-      expect(await toTopButton.isDisplayed()).to.be.true;
       await toTopButton.click();
       await driver.wait(until.elementIsNotVisible(toTopButton));
+      let waitTime = 2000;
+      while (
+        (await driver.executeScript("return window.scrollY;")) > 0 &&
+        waitTime > 0
+      ) {
+        await driver.manage().setTimeouts({ implicit: 200 });
+        waitTime -= 200;
+      }
       expect(await driver.executeScript("return window.scrollY;")).to.equal(0);
     });
   });
 
-  describe("For logged in users", async () => {
+  context("For logged in users", async () => {
     before(async () => {
       await driver.get("https://katalon-demo-cura.herokuapp.com/");
       try {
@@ -118,6 +143,18 @@ describe("Scroll to top Button", async () => {
           By.xpath('//*[@id="to-top"]')
         );
         expect(await toTopButton.isDisplayed()).to.be.false;
+      }
+    });
+    it("disappears after manually scrolling to top", async () => {
+      for (const url of [
+        "https://katalon-demo-cura.herokuapp.com/",
+        "https://katalon-demo-cura.herokuapp.com/history.php",
+        "https://katalon-demo-cura.herokuapp.com/profile.php",
+      ]) {
+        await driver.get(url);
+        const toTopButton = await driver.findElement(
+          By.xpath('//*[@id="to-top"]')
+        );
 
         await scrollDown(driver);
         await scrollToTop(driver);
@@ -135,8 +172,6 @@ describe("Scroll to top Button", async () => {
         const toTopButton = await driver.findElement(
           By.xpath('//*[@id="to-top"]')
         );
-        expect(await toTopButton.isDisplayed()).to.be.false;
-
         await scrollDown(driver);
         await driver.wait(until.elementIsVisible(toTopButton), 2000);
         await toTopButton.click();
@@ -154,19 +189,16 @@ describe("Scroll to top Button", async () => {
         const toTopButton = await driver.findElement(
           By.xpath('//*[@id="to-top"]')
         );
-        expect(await toTopButton.isDisplayed()).to.be.false;
 
         await scrollDown(driver);
         await driver.wait(until.elementIsVisible(toTopButton), 2000);
         await toTopButton.click();
-        await driver.wait(until.elementIsNotVisible(toTopButton), 2000);
-        await driver.wait(
-          until.elementIsVisible(await driver.findElement(By.css("header"))),
-          2000
-        );
-        expect(await toTopButton.isDisplayed()).to.be.false;
+        await driver.wait(until.elementIsNotVisible(toTopButton));
         let waitTime = 2000;
-        while ((await driver.executeScript("return window.scrollY;")) > 0) {
+        while (
+          (await driver.executeScript("return window.scrollY;")) > 0 &&
+          waitTime > 0
+        ) {
           await driver.manage().setTimeouts({ implicit: 200 });
           waitTime -= 200;
         }
